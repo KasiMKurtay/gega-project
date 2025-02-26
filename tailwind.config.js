7c36691be2c1c2d01c7f6821f67fac68078d468c
KasiMKurtay <kasmkurtay@outlook.com>
2025-02-26 17:08:00 +0300

    Updated: Tailwind CSS configuration updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      screens:{
+        lg: '1140px',
+        xl: '1140px',
+        "2xl":"1140px",
+      },
+    },
+    extend: {
+      fontFamily:{
+        gemunu: ["Gemunu Libre", "serif"],
+        open: ["Open Sans", 'serif'],
+      },
+      colors:{
+        "gega-red":"#BC1A45",
+        "gega-melon":"#FFD369",
+        "gega-grey":"#DDDDDD",
+        "gega-white":"#F7F7F7",
+      },
+    },
   },
   plugins: [],
 }
